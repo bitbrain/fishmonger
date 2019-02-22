92a2c5123b74844a2df8ed758644aacac5d9b92a
Miguel Gonzalez Sanchez <miguel-gonzalez@gmx.de>
2019-02-22 18:45:15 +0000

    Initial libgdx setup

---
+++ desktop/src/de/bitbrain/fishmonger/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.bitbrain.fishmonger.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.bitbrain.fishmonger.FishMongerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FishMongerGame(), config);
+	}
+}
