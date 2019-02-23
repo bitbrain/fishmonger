c2e313ee4d2ef96ea5d3e5dbca62e7855a855a12
Miguel Gonzalez Sanchez <miguel-gonzalez@gmx.de>
2019-02-23 09:53:12 +0000

    Improve visuals

--- core/assets/maps/tileset.tsx
+++ core/assets/maps/tileset.tsx
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
-<tileset name="tileset" tilewidth="8" tileheight="8" tilecount="98" columns="14">
- <image source="tileset.png" width="112" height="56"/>
+<tileset name="tileset" tilewidth="8" tileheight="8" spacing="2" margin="2" tilecount="40" columns="8">
+ <image source="tileset.png" width="82" height="52"/>
  <terraintypes>
-  <terrain name="GRASS" tile="8"/>
-  <terrain name="WATER" tile="23"/>
+  <terrain name="GRASS" tile="9"/>
+  <terrain name="WATER" tile="26"/>
  </terraintypes>
  <tile id="0" terrain="1,1,1,0">
   <properties>
@@ -13,7 +13,7 @@
  </tile>
  <tile id="1" terrain="1,1,0,0">
   <properties>
-   <property name="collision" type="bool" value="false"/>
+   <property name="collision" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="2" terrain="1,1,0,1">
@@ -21,52 +21,72 @@
    <property name="collision" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="3" terrain="0,0,0,0" probability="0.4"/>
- <tile id="4" terrain="0,0,0,0" probability="0.4"/>
- <tile id="5" terrain="0,0,0,0" probability="0.4"/>
- <tile id="14" terrain="1,0,1,0">
+ <tile id="3" terrain="0,0,0,0" probability="0.3"/>
+ <tile id="4" terrain="0,0,0,0" probability="0.3"/>
+ <tile id="5" terrain="0,0,0,0" probability="0.3"/>
+ <tile id="8" terrain="1,0,1,0">
   <properties>
    <property name="collision" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="15" terrain="0,0,0,0"/>
- <tile id="16" terrain="0,1,0,1">
+ <tile id="9" terrain="0,0,0,0">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="10" terrain="0,1,0,1">
   <properties>
    <property name="collision" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="17" terrain="0,0,0,0" probability="0.4"/>
- <tile id="18" terrain="0,0,0,0" probability="0.4"/>
- <tile id="19" terrain="0,0,0,0" probability="0.4"/>
- <tile id="28" terrain="1,0,1,1">
+ <tile id="11" terrain="0,0,0,0" probability="0.3"/>
+ <tile id="12" terrain="0,0,0,0" probability="0.3"/>
+ <tile id="13" terrain="0,0,0,0" probability="0.3"/>
+ <tile id="16" terrain="1,0,1,1">
   <properties>
    <property name="collision" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="29" terrain="0,0,1,1">
+ <tile id="17" terrain="0,0,1,1">
   <properties>
    <property name="collision" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="30" terrain="0,1,1,1">
+ <tile id="18" terrain="0,1,1,1">
   <properties>
    <property name="collision" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="42" terrain="0,0,0,1"/>
- <tile id="43" terrain="0,0,1,0"/>
- <tile id="44" terrain="1,1,1,1">
+ <tile id="24" terrain="0,0,0,1">
   <properties>
    <property name="collision" type="bool" value="true"/>
   </properties>
+ </tile>
+ <tile id="25" terrain="0,0,1,0">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="26" terrain="1,1,1,1">
   <animation>
-   <frame tileid="44" duration="500"/>
-   <frame tileid="45" duration="500"/>
-   <frame tileid="46" duration="500"/>
-   <frame tileid="47" duration="500"/>
-   <frame tileid="48" duration="500"/>
+   <frame tileid="26" duration="500"/>
+   <frame tileid="27" duration="500"/>
+   <frame tileid="28" duration="500"/>
+   <frame tileid="29" duration="500"/>
+   <frame tileid="30" duration="500"/>
+   <frame tileid="29" duration="500"/>
+   <frame tileid="28" duration="500"/>
+   <frame tileid="27" duration="500"/>
   </animation>
  </tile>
- <tile id="56" terrain="0,1,0,0"/>
- <tile id="57" terrain="1,0,0,0"/>
+ <tile id="32" terrain="0,1,0,0">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="33" terrain="1,0,0,0">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
--- core/src/de/bitbrain/fishmonger/screens/IngameScreen.java
+++ core/src/de/bitbrain/fishmonger/screens/IngameScreen.java
@@ -7,14 +7,13 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import de.bitbrain.braingdx.GameContext;
 import de.bitbrain.braingdx.assets.SharedAssetManager;
+import de.bitbrain.braingdx.behavior.movement.Orientation;
 import de.bitbrain.braingdx.behavior.movement.RasteredMovementBehavior;
-import de.bitbrain.braingdx.graphics.animation.AnimationConfig;
-import de.bitbrain.braingdx.graphics.animation.AnimationFrames;
-import de.bitbrain.braingdx.graphics.animation.AnimationRenderer;
-import de.bitbrain.braingdx.graphics.animation.AnimationSpriteSheet;
+import de.bitbrain.braingdx.graphics.animation.*;
 import de.bitbrain.braingdx.input.OrientationMovementController;
 import de.bitbrain.braingdx.screens.AbstractScreen;
 import de.bitbrain.braingdx.tmx.TiledMapType;
+import de.bitbrain.braingdx.util.Enabler;
 import de.bitbrain.braingdx.world.GameObject;
 import de.bitbrain.braingdx.world.SimpleWorldBounds;
 import de.bitbrain.fishmonger.Colors;
@@ -38,7 +37,6 @@ public class IngameScreen extends AbstractScreen<FishMongerGame> {
    @Override
    protected void onCreate(GameContext context) {
       context.getAudioManager().playMusic(Assets.Musics.OVERWORLD);
-      setBackgroundColor(Colors.BACKGROUND);
       setupWorld(context);
       setupInput(context);
       setupRenderer(context);
@@ -74,7 +72,7 @@ public class IngameScreen extends AbstractScreen<FishMongerGame> {
 
       context.getRenderManager().register("PLAYER", new AnimationRenderer(playerSheet,
             AnimationConfig.builder()
-                  .registerFrames("PLAYER", AnimationFrames.builder()
+                  .registerFrames(Orientation.DOWN, AnimationFrames.builder()
                         .resetIndex(0)
                         .duration(0.2f)
                         .origin(0, 0)
@@ -82,12 +80,47 @@ public class IngameScreen extends AbstractScreen<FishMongerGame> {
                         .playMode(Animation.PlayMode.LOOP_REVERSED)
                         .frames(8)
                         .build())
+                  .registerFrames(Orientation.UP, AnimationFrames.builder()
+                        .resetIndex(0)
+                        .duration(0.2f)
+                        .origin(0, 1)
+                        .direction(AnimationFrames.Direction.HORIZONTAL)
+                        .playMode(Animation.PlayMode.LOOP_REVERSED)
+                        .frames(8)
+                        .build())
+                  .registerFrames(Orientation.RIGHT, AnimationFrames.builder()
+                        .resetIndex(0)
+                        .duration(0.2f)
+                        .origin(0, 2)
+                        .direction(AnimationFrames.Direction.HORIZONTAL)
+                        .playMode(Animation.PlayMode.LOOP_REVERSED)
+                        .frames(8)
+                        .build())
+                  .registerFrames(Orientation.LEFT, AnimationFrames.builder()
+                        .resetIndex(0)
+                        .duration(0.2f)
+                        .origin(0, 3)
+                        .direction(AnimationFrames.Direction.HORIZONTAL)
+                        .playMode(Animation.PlayMode.LOOP_REVERSED)
+                        .frames(8)
+                        .build())
                   .build()
-      ));
+            , new AnimationTypeResolver<GameObject>() {
+         @Override
+         public Object getAnimationType(GameObject object) {
+            return object.getAttribute(Orientation.class);
+         }
+      }, new Enabler<GameObject>() {
+         @Override
+         public boolean isEnabledFor(GameObject target) {
+            return target.getOffsetX() > 0 || target.getOffsetY() > 0;
+         }
+      }).offset(0f, 4f));
    }
 
    private void configurePlayer(GameContext context, GameObject player) {
-      player.setDimensions(16f, 16f);
+      player.setDimensions(16f, 8f);
+      player.setScaleY(2f);
       float correctX = (float) (Math.floor(player.getLeft() / context.getTiledMapManager().getAPI().getCellWidth()) * context.getTiledMapManager().getAPI().getCellWidth());
       float correctY = (float) (Math.floor(player.getTop() / context.getTiledMapManager().getAPI().getCellHeight()) * context.getTiledMapManager().getAPI().getCellHeight());
       player.setPosition(correctX, correctY);
@@ -95,12 +128,12 @@ public class IngameScreen extends AbstractScreen<FishMongerGame> {
       context.getGameCamera().setDefaultZoomFactor(0.1f);
       context.getGameCamera().setZoomScalingFactor(0.0000001f);
       context.getGameCamera().setTrackingTarget(player);
-      context.getGameCamera().setTargetTrackingSpeed(0.05f);
+      context.getGameCamera().setTargetTrackingSpeed(0.1f);
       player.setOrigin(player.getWidth() / 2f, player.getHeight() / 2f);
 
       OrientationMovementController controller = new OrientationMovementController();
       final RasteredMovementBehavior behavior = new RasteredMovementBehavior(controller, context.getTiledMapManager().getAPI())
-            .interval(0.35f)
+            .interval(0.15f)
             .rasterSize(context.getTiledMapManager().getAPI().getCellWidth(), context.getTiledMapManager().getAPI().getCellHeight());
       context.getBehaviorManager().apply(behavior, player);
    }
