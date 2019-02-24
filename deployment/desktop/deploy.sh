6dd9a1c5c137c729be702d5b8f5f8a325fa59fec
Miguel Gonzalez Sanchez <miguel-gonzalez@gmx.de>
2019-02-24 15:22:34 +0000

    Do not release Mac anymore

--- deployment/desktop/deploy.sh
+++ deployment/desktop/deploy.sh
@@ -47,7 +47,7 @@ prepare_butler
 prepare_packr
 
 prepare_and_push "windows" "packr-windows.json" "openjdk-1.7.0-u80-unofficial-windows-i586-image.zip"
-prepare_and_push "mac" "packr-mac.json" "openjdk-1.7.0-u80-unofficial-macosx-x86_64-image.zip" ".app"
+#prepare_and_push "mac" "packr-mac.json" "openjdk-1.7.0-u80-unofficial-macosx-x86_64-image.zip" ".app"
 
 echo "Done."
 exit 0
