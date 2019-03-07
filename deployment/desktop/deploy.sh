7ad804e0a65e87bbbc9ec2d8b93fef650c06e195
Miguel Gonzalez Sanchez <miguel-gonzalez@gmx.de>
2019-03-07 19:12:49 +0000

    Fix deployment

--- deployment/desktop/deploy.sh
+++ deployment/desktop/deploy.sh
@@ -29,7 +29,22 @@ prepare_and_push() {
     download_if_not_exist https://bitbucket.org/alexkasko/openjdk-unofficial-builds/downloads/$3 $3
     java -jar packr.jar --jdk $3 --output fishmonger-$1$4 -- deployment/desktop/$2
 
-    zip -r fishmonger-$1.zip fishmonger-$1$4
+    zip -r fishmonger-$1$4.zip fishmonger-$1$4
+    rm -rf fishmonger-$1$4
+
+    echo "Push $1 build to itch.io..."
+    ./butler push fishmonger-$1$4.zip bitbrain/fishmonger:$1
+}
+
+prepare_and_push_raw() {
+    echo "Preparing $1 build..."
+    rm -rf fishmonger-$1
+    mkdir fishmonger-$1
+    mkdir fishmonger-$1/assets
+    cp -rf core/assets/* fishmonger-$1/assets
+    cp fishmonger.jar fishmonger-$1
+
+    zip -r fishmonger-$1.zip fishmonger-$1
     rm -rf fishmonger-$1
 
     echo "Push $1 build to itch.io..."
@@ -47,7 +62,7 @@ prepare_butler
 prepare_packr
 
 prepare_and_push "windows" "packr-windows.json" "openjdk-1.7.0-u80-unofficial-windows-i586-image.zip"
-#prepare_and_push "mac" "packr-mac.json" "openjdk-1.7.0-u80-unofficial-macosx-x86_64-image.zip" ".app"
+prepare_and_push_raw "windows-mac-linux"
 
 echo "Done."
 exit 0
